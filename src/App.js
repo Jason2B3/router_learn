8694ba611b0cf0c911161e0600e7dd0b7a97c541
jason <2B3>
2021-09-13 02:16:58 -0400

    commit 274

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
@@ -9,19 +9,17 @@ function App() {
       <MainHeader />
 
       <Switch>
+        <Route path="/" exact>
+          {/*needs the exact attrib, since every path begins with "/"*/}
+          <Redirect to="/welcome" />
+        </Route>
         <Route path="/welcome">
-          {" "}
-          ROUTE 1
           <Welcome />
         </Route>
         <Route path="/products" exact>
-          {" "}
-          ROUTE 2
           <Products />
         </Route>
         <Route path="/products/:productID">
-          {" "}
-          ROUTE 3
           <ProductDetail />
         </Route>
       </Switch>
